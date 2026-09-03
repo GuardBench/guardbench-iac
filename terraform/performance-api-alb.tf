2f415f1d058f8789902054b57ac2b509117eb1e9
Haein Choe <sodychoe@gmail.com>
2026-09-04 02:55:08 +0900

    fix(terraform): allow performance target access from API

--- terraform/performance-api-alb.tf
+++ terraform/performance-api-alb.tf
@@ -22,6 +22,16 @@ resource "aws_security_group_rule" "performance_api_alb_ingress_from_runner" {
   security_group_id        = aws_security_group.performance_api_alb.id
 }
 
+resource "aws_security_group_rule" "performance_api_alb_ingress_from_api" {
+  type                     = "ingress"
+  from_port                = 80
+  to_port                  = 80
+  protocol                 = "tcp"
+  source_security_group_id = aws_security_group.api.id
+  description              = "Performance Backend target calls through the internal ALB"
+  security_group_id        = aws_security_group.performance_api_alb.id
+}
+
 resource "aws_security_group_rule" "performance_api_alb_egress_to_api" {
   type                     = "egress"
   from_port                = var.api_container_port
